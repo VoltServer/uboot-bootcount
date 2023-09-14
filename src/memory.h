a070404fd1d19c52c21403576162c73006a9d4e7
Dario Binacchi <dario.binacchi@amarulasolutions.com>
2023-09-14 17:41:54 +0200

    Group duplicated source code related to memory access
    Following the software principle 'Don't repeat yourself,' the patch groups
    the source code for register access, where reading/writing the bootcount
    value, into a single function with parameters to make it usable across
    different architectures.
    
    Signed-off-by: Dario Binacchi <dario.binacchi@amarulasolutions.com>

---
+++ src/memory.h
@@ -0,0 +1,25 @@
+/**
+ * Memory access
+ *
+ * This file is part of the uboot-bootcount (https://github.com/VoltServer/uboot-bootcount).
+ *
+ * Copyright (c) 2023 Amarula Solutions, Dario Binacchi <dario.binacchi@amarulasolutions.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+# pragma once
+
+#include <sys/types.h>
+
+void *memory_open(off_t offset, size_t len);
